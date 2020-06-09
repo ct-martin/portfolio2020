1b97c7d372259c8f7c70e9d5a6cd05cccf7185be
Christian Martin <ct-martin@users.noreply.github.com>
2020-06-09 17:55:51 -0400

    Initial commit; Base Nuxt app

---
+++ README.md
@@ -0,0 +1,12 @@
+# Portfolio
+
+Curated showcase of what I've done (2020 rewrite).
+
+[![pipeline status](https://gitlab.com/ctmartin/portfolio2020/badges/master/pipeline.svg)](https://gitlab.com/ctmartin/portfolio2020/-/commits/master)
+
+## Built using
+
+* [Nuxt.js](https://nuxtjs.org/)
+* [TypeScript](https://www.typescriptlang.org/) with type checking enforced
+* [SASS](https://sass-lang.com/)
+* [GitLab CI](https://about.gitlab.com/ci-cd/)
---
+++ nuxt.config.js
@@ -0,0 +1,46 @@
+export default {
+  build: {
+    extend (config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(.js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  buildModules: ['@nuxt/typescript-build'],
+  css: [
+    '@/assets/theme/assets/css/main.scss'
+  ],
+  generate: {
+    dir: 'public',
+    fallback: true
+  },
+  head: {
+    bodyAttrs: {
+      id: '__nuxt_body'
+    },
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'description', content: 'Christian Martin\'s Portfolio' }
+    ],
+    titleTemplate: 'Portfolio | Christian Martin'
+  },
+  loading: {
+    color: 'black'
+  },
+  mode: 'spa',
+  modern: 'client',
+  typescript: {
+    typeCheck: {
+      eslint: true
+    }
+  }
+}
