0ef7b9e3ec912dbb3ac215a38d06a34de10ece2d
Christian Martin <ct-martin@users.noreply.github.com>
2020-07-09 18:50:05 -0400

    Update dependencies

---
+++ pages/projects/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <main>
+    <v-container fluid>
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col cols="12" md="10" class="py-0">
+          <v-container fluid>
+            <h1 class="display-1 mb-0">
+              Projects
+            </h1>
+          </v-container>
+        </v-col>
+      </v-row>
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col cols="12" md="10" class="pt-0">
+          <v-container fluid class="pt-0">
+            <v-row
+              align="stretch"
+              justify="center"
+            >
+              <v-col cols="12" md="6" lg="4">
+                <v-card tile>
+                  <v-card-title>
+                    Find
+                  </v-card-title>
+                  <v-card-text class="text--primary">
+                    Single-Page App Search Engine for APIs using Schema.org markup.
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn text href="https://find.ctmartin.me/" target="_blank" rel="noopener" :ripple="false">
+                      View
+                    </v-btn>
+                    <v-btn text nuxt to="/projects/find" :ripple="false">
+                      Learn More
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-col>
+              <v-col cols="12" md="6" lg="4" offset-lg="1">
+                <v-card tile>
+                  <v-card-title>
+                    SI
+                  </v-card-title>
+                  <v-card-text class="text--primary">
+                    Interactive visualization of the Smithsonian's 11 million-record Open Access dataset.
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn text href="https://si.ctmartin.dev/" target="_blank" rel="noopener" :ripple="false">
+                      View
+                    </v-btn>
+                    <v-btn text nuxt to="/projects/si" :ripple="false">
+                      Learn More
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
+  </main>
+</template>
---
+++ pages/projects/vis.vue
@@ -0,0 +1,63 @@
+<template>
+  <main>
+    <v-container fluid>
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col cols="12" md="10" class="pt-0">
+          <v-container fluid>
+            <h1 class="display-1 mb-4">
+              Name
+            </h1>
+            <p>
+              What &amp; Background
+            </p>
+            <p>
+              How
+            </p>
+            <p>
+              Tech
+            </p>
+            <p class="mb-0">
+              Usage
+            </p>
+          </v-container>
+        </v-col>
+      </v-row>
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col cols="12" md="10" class="py-0">
+          <v-container fluid class="pt-0 text-center">
+            <v-btn text href="" :ripple="false">
+              View
+              <v-icon dark small>
+                mdi-arrow-right
+              </v-icon>
+            </v-btn>
+            <v-btn text href="" :ripple="false">
+              GitHub
+              <v-icon dark small>
+                mdi-arrow-right
+              </v-icon>
+            </v-btn>
+            <v-btn text href="" :ripple="false">
+              Write-Up
+              <v-icon dark small>
+                mdi-arrow-right
+              </v-icon>
+            </v-btn>
+            <v-btn text href="" :ripple="false">
+              Other Write-Up
+              <v-icon dark small>
+                mdi-arrow-right
+              </v-icon>
+            </v-btn>
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
+  </main>
+</template>
