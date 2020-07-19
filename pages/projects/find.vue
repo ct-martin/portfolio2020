570e6d45098155750962f86f91ad420feb85de6d
Christian Martin <ct-martin@users.noreply.github.com>
2020-07-18 17:23:58 -0700

    Add Find Project Page

--- pages/projects/find.vue
+++ pages/projects/find.vue
@@ -11,16 +11,18 @@
               Find
             </h1>
             <p>
-              What &amp; Background
+              Find is a search engine based on Schema.org markup.
             </p>
+            <v-img alt="Screenshot of application" src="/img/projects/find.png" max-height="400px" contain class="mb-4" />
             <p>
-              How
-            </p>
-            <p>
-              Tech
+              Find consumes the supported subset of Schema.org markup in the JSON-LD format and recursively iterates through it to find pages.
+              A summary of data is created and displayed on the page.
+              Filters are available for sites and page types (such as Blog Posting or Recipe) and a search box is available for the title and description.
+              The search also has string normalization, including "smart" typography (such as quotes and em-dashes) and Unicode characters.
             </p>
             <p class="mb-0">
-              Usage
+              Find is a Single-Page App built in Nuxt (from the Vue ecosystem), TypeScript, and SASS.
+              A fetcher queries all APIs given and then passes them to a parser, which simplifies the Schema.org markup into something usable.
             </p>
           </v-container>
         </v-col>
@@ -31,26 +33,32 @@
       >
         <v-col cols="12" md="10" class="py-0">
           <v-container fluid class="pt-0 text-center">
-            <v-btn text href="" :ripple="false">
+            <v-btn text href="https://find.ctmartin.me/" target="_blank" rel="noopener" :ripple="false">
               View
               <v-icon dark small>
                 mdi-arrow-right
               </v-icon>
             </v-btn>
-            <v-btn text href="" :ripple="false">
+            <v-btn text href="https://github.com/ct-martin/find.ctmartin.me" target="_blank" rel="noopener" :ripple="false">
               GitHub
               <v-icon dark small>
                 mdi-arrow-right
               </v-icon>
             </v-btn>
-            <v-btn text href="" :ripple="false">
-              Write-Up
+            <v-btn text href="https://github.com/ct-martin/find.ctmartin.me/blob/master/README.md" target="_blank" rel="noopener" :ripple="false">
+              Find Write-Up
               <v-icon dark small>
                 mdi-arrow-right
               </v-icon>
             </v-btn>
-            <v-btn text href="" :ripple="false">
-              Other Write-Up
+            <v-btn text href="https://vis.ctmartin.me/api-in-mermaid/#more-mermaid" target="_blank" rel="noopener" :ripple="false">
+              Find-based Visualizations
+              <v-icon dark small>
+                mdi-arrow-right
+              </v-icon>
+            </v-btn>
+            <v-btn text href="https://blog.ctmartin.me/2020/04/schema-api-and-search/" target="_blank" rel="noopener" :ripple="false">
+              API Building Write-Up
               <v-icon dark small>
                 mdi-arrow-right
               </v-icon>
