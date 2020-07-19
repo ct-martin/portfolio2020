13523c54a5a0999c24d98ed74149d185b5136273
Christian Martin <ct-martin@users.noreply.github.com>
2020-07-18 17:24:12 -0700

    Update home & projects cards

--- pages/index.vue
+++ pages/index.vue
@@ -70,39 +70,25 @@
               justify="center"
             >
               <v-col cols="12" md="6" lg="4">
-                <v-card tile>
+                <v-card to="/projects/find" nuxt tile>
+                  <v-img src="/img/projects/find-card.jpg" height="200px" />
                   <v-card-title>
                     Find
                   </v-card-title>
                   <v-card-text class="text--primary">
                     Single-Page App Search Engine for APIs using Schema.org markup.
                   </v-card-text>
-                  <v-card-actions>
-                    <v-btn text href="https://find.ctmartin.me/" target="_blank" rel="noopener" :ripple="false">
-                      View
-                    </v-btn>
-                    <v-btn text nuxt to="/projects/find" :ripple="false">
-                      Learn More
-                    </v-btn>
-                  </v-card-actions>
                 </v-card>
               </v-col>
               <v-col cols="12" md="6" lg="4" offset-lg="1">
-                <v-card tile>
+                <v-card to="/projects/si" nuxt tile>
+                  <v-img src="/img/projects/si-card.jpg" height="200px" />
                   <v-card-title>
                     SI
                   </v-card-title>
                   <v-card-text class="text--primary">
                     Interactive visualization of the Smithsonian's 11 million-record Open Access dataset.
                   </v-card-text>
-                  <v-card-actions>
-                    <v-btn text href="https://si.ctmartin.dev/" target="_blank" rel="noopener" :ripple="false">
-                      View
-                    </v-btn>
-                    <v-btn text nuxt to="/projects/si" :ripple="false">
-                      Learn More
-                    </v-btn>
-                  </v-card-actions>
                 </v-card>
               </v-col>
             </v-row>
