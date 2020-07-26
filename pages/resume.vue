c498d8b86fa0da835b129238022755c9790272f0
Christian Martin <ct-martin@users.noreply.github.com>
2020-07-26 15:47:01 -0700

    Add contact to resume

--- pages/resume.vue
+++ pages/resume.vue
@@ -15,15 +15,29 @@
         </v-col>
       </v-row>
     </v-container>
+    <v-container fluid class="green accent-4 py-5">
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col cols="12" md="10" xl="8">
+          <v-container fluid>
+            <Contact />
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
   </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Contact from '~/components/contact.vue'
 import Embed from '~/components/embed.vue'
 
 export default Vue.extend({
   components: {
+    Contact,
     Embed
   }
 })
